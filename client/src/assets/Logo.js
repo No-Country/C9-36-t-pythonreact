3177d9064c2e3408862b2d2836bbc6cf8e25ecfe
illiCristian <svrk73@gmail.com>
2023-02-06 23:40:43 -0300

    changes added

---
+++ client/src/assets/Logo.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <svg
+      width="105"
+      height="101"
+      viewBox="0 0 105 101"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M0.0175328 58.8418L20.368 22.3691L61.0385 22.3858L81.3586 58.8752L61.0082 95.3479L20.3376 95.3312L0.0175328 58.8418Z"
+        fill="#14AE5C"
+      />
+      <path
+        d="M23.6416 42.1247L43.992 5.65197L84.6625 5.66865L104.983 42.158L84.6322 78.6307L43.9616 78.6141L23.6416 42.1247Z"
+        fill="#51659A"
+      />
+    </svg>
+  );
+};
+
+export default Logo;
