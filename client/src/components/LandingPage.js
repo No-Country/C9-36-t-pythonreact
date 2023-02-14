0c040aeec9a8246637fbf0467ce6610ce50874d9
Tomas <tomaasvillagra@gmail.com>
2023-02-14 17:54:20 -0300

    LandingPage version mobile y desktop terminado, completamente responsive

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,28 +1,42 @@
 import React from "react";
 import Logo from "../assets/Logo";
 import { Link, NavLink } from "react-router-dom";
-import styles from "./Inicio.module.css";
+import styles from "./LandingPage.module.css";
 
-function Inicio() {
+
+function LandingPage() {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>
-        <NavLink to={"/"}>
-          <Logo />
-        </NavLink>
+      <div className={styles.containerLogoTitulo}>
+        <div className={styles.logo}>
+          <NavLink to={"/"}>
+            <Logo className={styles.logoSvg}/>
+          </NavLink>
+        </div>
+
+        <h1 className={styles.titulo}>
+          Te damos la bienvenida  a la comunidad de  developers más grande de  LATAM.
+        </h1>
+      </div>
+      
+      <div className={styles.photodevs}>
+        
+        <div className={styles.photodevsFondo}>
+        
+          <div className={styles.boton}>
+
+            <Link to={"/home"}>
+              <button className={styles.button}>Continuar</button>
+            </Link>
+
+          </div>
+
+        </div>
+
       </div>
 
-      <h1 className={styles.titulo}>
-        Te damos la bienvenida a la comunidad de developers más grande de LATAM
-      </h1>
-
-      <div className={styles.boton}>
-        <Link to={"/home"}>
-          <button>Continuar</button>
-        </Link>
-      </div>
     </div>
   );
 }
 
-export default Inicio;
+export default LandingPage;
