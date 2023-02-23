33df2e8c432e37e3b94e6d8340ee835e4513005e
Tomas <tomaasvillagra@gmail.com>
2023-02-23 15:24:16 -0300

    se crearon los componentes de perfiles y esta avanzada la dashboard mobile

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,26 +1,59 @@
-import React from "react";
-import { logOut } from "../config/firebase";
+import React, {useState} from "react";
+import Logo from "../assets/LogoDashboard";
+import { Link, NavLink } from "react-router-dom";
+// import { logOut } from "../config/firebase";
+import styles from "./Dashboard.module.css";
+import Navbar from './Navbar';
+import PerfilesBackend from './perfiles/PerfilesBackend';
+import PerfilesFrontend from './perfiles/PerfilesFrontend';
+import PerfilesDesigner from './perfiles/PerfilesDesigner';
+
 
 const Dashboard = () => {
-  const handleLogout = async () => {
+  {/**const handleLogout = async () => {
     try {
       await logOut();
     } catch (error) {
       console.log(error);
     }
+  };*/}
+  const [seleccion, setSeleccion] = useState('frontend');
+
+  const handleSeleccion = (opcion) => {
+    setSeleccion(opcion);
   };
+
+
   return (
-    <div className="flex justify-center flex-col">
-      <h1>Soy un Dashboard!!!!!!!!!!!!!!</h1>
-      <h3 className="text-pink-600 font-extrabold text-center mt-4 bg-teal-400">
-        --- Seccion en construccion --- Esta es la primer ruta protegida donde
-        ingresa el usuario despues de loguearse
-      </h3>
-      <button
-        onClick={handleLogout}
-        className="mt-10 py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-        Logout{" "}
-      </button>
+    <div>
+      <header>
+
+        <div>
+          <NavLink to={"/"} className={styles.logo}>
+              <Logo />
+          </NavLink>
+          <Link><button>Iniciar Sesión</button></Link>
+        </div>
+
+        <div className={styles.navPerfiles}>
+          <Link onClick={() => handleSeleccion('frontend')}>Frontend Dev´s</Link>
+          <Link onClick={() => handleSeleccion('backend')}>Backend Dev´s</Link>
+          <Link onClick={() => handleSeleccion('designer')}>Designers</Link>
+        </div>
+
+      </header>
+      
+      <section>
+        <h1>Buscá con quien trabajar</h1>
+
+        {/* Aquí se renderiza el contenido correspondiente a la opción seleccionada */}
+      {seleccion === 'frontend' && <PerfilesFrontend />}
+      {seleccion === 'backend' && <PerfilesBackend />}
+      {seleccion === 'designer' && <PerfilesDesigner />}
+
+      </section>
+      
+      <Navbar/>
     </div>
   );
 };
